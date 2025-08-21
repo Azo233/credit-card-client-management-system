17c267376b019306eca17bc06c6d285942a5a8cb
Andrija Azinić <azinic1999@gmail.com>
2025-08-21 18:50:25 +0200

    first commit

---
+++ src/main/java/com/example/Credit/Card/system/Credit_Card_Client_Management_System/CreditCardClientManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Credit.Card.system.Credit_Card_Client_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardClientManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardClientManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Credit/Card/system/Credit_Card_Client_Management_System/CreditCardClientManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Credit.Card.system.Credit_Card_Client_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardClientManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
